21012f01b92111c503b64d7546766134a02f42d0
Benjamin Bostick <40671731+elbenoss@users.noreply.github.com>
2024-05-04 21:04:52 -0400

    Add easy 1 exercises

---
+++ py101/exercises/easy-1/how-big-is-the-room.py
@@ -0,0 +1,4 @@
+length = int(input("Enter to length of the room in meters: "))
+width = int(input("Enter the width of the room in meters: "))
+dimensions = length * width
+print(f"The room's area in square meters: {dimensions}\nThe room's area in square feet: {dimensions*10.7639}")
